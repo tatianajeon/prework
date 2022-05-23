36f1646effb849b0a2f66509d1302de6ca56b7d4
tatiana <105023527+tatianajeon@users.noreply.github.com>
2022-05-23 16:34:04 -0500

    Add files via upload

--- python prework assignment.py
+++ python prework assignment.py
@@ -32,17 +32,15 @@ max_num_in_list(a_list)
 # Write a function to return if the given year is a leap year. 
 # A leap year is divisible by 4, but not divisible by 100 unless it is also divisible by 400. 
 # The return should be boolean Type (true/false). 
+
 def is_leap_year(a_year):
-    if a_year % 4 == 0:
-        if a_year % 400:
-            print(f"{a_year} is a leap year.")
-            if a_year % 100 == 1:
-                print(f"{a_year} is not a leap year.")
-    else: 
-        print(f"{a_year} is not a leap year.")
-        
-is_leap_year(2022)
-is_leap_year(2024)
+    if a_year % 4 == 0 and a_year % 100 != 0 or a_year % 400 == 0:
+        return True
+    else:
+        return False
+
+print(is_leap_year(2022))
+
 
 
 # Question 5
@@ -50,16 +48,9 @@ is_leap_year(2024)
 # For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. 
 # The return should be boolean Type. 
 def is_consecutive(a_list):
-    while True:
-        for x in a_list:
-            y = a_list.index(x+1)
-            if y + 1 == a_list.index(x+2):
-                print("This is a consecutive list.")
-                if x == a_list.index[-1]:
-                    break
-            elif y != a_list.index(x+1):
-                return False
+    return sorted (a_list) == list(range(min(a_list), max(a_list)+1))
 
 a_list = [19, 11, 20, 13, 14, 15] 
+a_list = [19, 20, 21, 22, 23, 24] 
 
-is_consecutive(a_list)
+print(is_consecutive(a_list))
